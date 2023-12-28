6fbc1f5f360dec5724fa4d20603fb6f4382cc51c
Sam Scott <samoscarscott@gmail.com>
2023-12-28 17:32:29 +0000

    slightly update styling

--- src/components/CommissionCalculator/CommissionBreakdown.tsx
+++ src/components/CommissionCalculator/CommissionBreakdown.tsx
@@ -21,10 +21,10 @@ export const CommissionBreakdown = ({
   console.log(breakdown);
 
   return (
-    <div className="mt-8 p-4 border border-gray-300 bg-gray-50 rounded-lg flex flex-col gap-12">
+    <div className="mt-8 p-4 border border-gray-300 bg-gray-50 rounded-lg flex flex-col gap-8">
       <div>
-        <p className="font-semibold mb-4">Total commission</p>
-        <p className="text-4xl font-bold text-center">
+        <p className="font-semibold mb-2">Total commission</p>
+        <p className="text-5xl font-bold text-left">
           £<CountUp end={total} preserveValue />
         </p>
       </div>
@@ -67,6 +67,7 @@ export const CommissionBreakdown = ({
                       £
                       <CountUp
                         end={breakdownItem.commissionValue}
+                        decimals={2}
                         preserveValue
                       />
                     </p>
@@ -76,38 +77,47 @@ export const CommissionBreakdown = ({
             )}
           </div>
         </div>
-        <div className="flex-1 flex flex-col gap-4">
-          <p className="font-semibold">Commission structure</p>
-          <ul className="flex flex-wrap flex-col gap-2 flex-1 justify-center">
-            {commissionStructure.map((structureItem, i) => {
-              const min = structureItem.band[0];
+        <div className="flex-1 flex flex-col gap-4  justify-between">
+          <div>
+            <p className="font-semibold">Commission structure</p>
+          </div>
 
-              const max =
-                structureItem.band[1] === Infinity
-                  ? "+"
-                  : structureItem.band[1];
-              const color = colors[i % colors.length];
+          <div>
+            <ul className="flex flex-wrap flex-col gap-2 flex-1 justify-center">
+              {commissionStructure.map((structureItem, i) => {
+                const min = structureItem.band[0];
 
-              const labelString =
-                structureItem.band[1] !== Infinity
-                  ? `£${min.toLocaleString()}-${max.toLocaleString()}`
-                  : `£${min.toLocaleString()}+`;
+                const max =
+                  structureItem.band[1] === Infinity
+                    ? "+"
+                    : structureItem.band[1];
+                const color = colors[i % colors.length];
 
-              return (
-                <li
-                  key={`${min}-${max}`}
-                  className="flex gap-1 items-center text-sm"
-                >
-                  <div
-                    className="w-4 h-4 rounded"
-                    style={{ backgroundColor: color }}
-                  />
-                  <p className="flex-1">{labelString}</p>
-                  <p>{structureItem.commissionPercentage}%</p>
-                </li>
-              );
-            })}
-          </ul>
+                const labelString =
+                  structureItem.band[1] !== Infinity
+                    ? `£${min.toLocaleString()}-${max.toLocaleString()}`
+                    : `£${min.toLocaleString()}+`;
+
+                return (
+                  <li
+                    key={`${min}-${max}`}
+                    className="flex gap-1 items-center text-sm"
+                  >
+                    <div
+                      className="w-4 h-4 rounded"
+                      style={{ backgroundColor: color }}
+                    />
+                    <p className="flex-1">{labelString}</p>
+                    <p>{structureItem.commissionPercentage}%</p>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="flex justify-between text-xs text-gray-500 font-light italic mt-1">
+              <p>Band</p>
+              <p>Commission percentage</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
